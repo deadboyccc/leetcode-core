30a96a2ccfb9ce500247d17a29923d3225452b8c
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-13 19:51:43 +0300

    283. Move Zeroes

--- kotlin-solutions/src/283.kt
+++ kotlin-solutions/src/283.kt
@@ -22,7 +22,28 @@ class Solution {
     }
 }
 
-class improvedSolution
+
+// at cost of mem
+class ImprovedSolution {
+    fun moveZeroes(nums: IntArray): Unit {
+        val zeroCount = nums.count { it == 0 }
+        if (zeroCount == 0) return
+
+        val arr = (nums.filter { it != 0 } + IntArray(zeroCount) { 0 }.toList()).toIntArray()
+        arr.copyInto(nums)
+    }
+}
+
+// most optimal tp solution
+class SolutionBrute {
+    fun moveZeroes(nums: IntArray): Unit {
+        var insert = 0
+        for (i in nums.indices) {
+            if (nums[i] != 0) nums[insert++] = nums[i]
+        }
+        while (insert < nums.size) nums[insert++] = 0
+    }
+}
 
 fun main() {
     Solution().moveZeroes(intArrayOf(8, 10, 0, 20, 0, 50, 0))
