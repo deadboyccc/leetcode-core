bdec3cdefc53dea2fed67a92fb8842c8118de667
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-14 21:11:10 +0300

    75 sort colors

---
+++ kotlin-solutions/src/review.kt
@@ -0,0 +1,13 @@
+package review;
+
+class Solution {
+    fun allPathsSourceTarget(graph: Array<IntArray>): List<List<Int>> {
+
+        val adj = graph.withIndex().associate { it.index to it.value }
+
+//        fun dfs()
+
+
+        return emptyList()
+    }
+}
