ce9bc9d9a8d69d468cbb19bec6a2630e806e46d0
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-17 23:24:23 +0300

    54. Spiral Matrix

---
+++ kotlin-solutions/src/54.kt
@@ -0,0 +1,13 @@
+package five4
+
+class Solution {
+    fun spiralOrder(matrix: Array<IntArray>): List<Int> {
+        // go right if boundary -> travel downward -> leftward->upward -> ..
+        // ea
+        // TODO
+
+        return emptyList()
+
+
+    }
+}
