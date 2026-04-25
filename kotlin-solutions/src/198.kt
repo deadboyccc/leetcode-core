fbefa480e6de4fb846554fc403b1da95a188c1d6
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-25 18:35:15 +0300

    198. House Robber

---
+++ kotlin-solutions/src/198.kt
@@ -0,0 +1,46 @@
+package one98
+
+class ReadableSolution {
+    fun rob(nums: IntArray): Int =
+        nums.fold(0 to 0) { (twoBack, oneBack), houseValue ->
+
+            // slide the window: twoBack is discarded, oneBack becomes the new twoBack
+            val newTwoBack = oneBack
+            // either skip this house (oneBack) or rob it (twoBack + houseValue)
+            val newOneBack = maxOf(oneBack, twoBack + houseValue)
+
+            newTwoBack to newOneBack
+        }.second
+}
+
+class SolutionMemo {
+    fun rob(nums: IntArray): Int {
+        val memo = mutableMapOf<Int, Int>()
+        fun dfs(i: Int): Int {
+            if (i !in nums.indices) return 0
+            return memo.getOrPut(i) {
+                maxOf(dfs(i + 1), nums[i] + dfs(i + 2))
+            }
+        }
+        return dfs(0)
+    }
+}
+
+// non mem
+class dfsSolution {
+    // dfs = sum , out of index = 0
+    fun rob(nums: IntArray): Int {
+        fun dfs(i: Int): Int {
+            if (i !in nums.indices) return 0
+            return maxOf(dfs(i + 1), nums[i] + dfs(i + 2))
+        }
+        return dfs(0)
+    }
+}
+
+class Solution {
+    fun rob(nums: IntArray): Int =
+        nums.fold(0 to 0) { (prev2, prev1), n ->
+            prev1 to maxOf(prev1, prev2 + n)
+        }.second
+}
