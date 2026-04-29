8c471af46e079de5307df733f3b8daa996434bc1
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-29 16:40:33 +0300

    509. Fibonacci Number

---
+++ kotlin-solutions/src/509.kt
@@ -0,0 +1,17 @@
+package five09
+
+class Solution {
+    fun fib(n: Int): Int {
+        if (n == 0) return 0
+        if (n == 1) return 1
+        // fib(0) =0 , f(1) = 1
+        return (2..n).fold(0 to 1) { (minusTwo, minusOne), i ->
+            // minusTwo to minusOne
+
+            minusOne to minusOne + minusTwo
+
+        }.second
+
+
+    }
+}
