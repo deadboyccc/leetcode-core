7fe2db13663ab9cc2959089ee8d13864ab5a271c
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-04-30 16:07:51 +0300

    1137. N-th Tribonacci Number

--- kotlin-solutions/src/1137.kt
+++ kotlin-solutions/src/1137.kt
@@ -1,5 +1,27 @@
 package one137;
 
+class OptimizedSolution {
+    fun tribonacci(n: Int): Int {
+        if (n == 0) return 0
+        if (n == 1 || n == 2) return 1
+
+        var minusThree = 0
+        var minusTwo = 1
+        var minusOne = 1
+
+        for (i in 3..n) {
+            val currTrib = minusThree + minusTwo + minusOne
+
+            // Shift the values forward
+            minusThree = minusTwo
+            minusTwo = minusOne
+            minusOne = currTrib
+        }
+
+        return minusOne
+    }
+}
+
 class Solution {
     val memo = mutableMapOf<Int, Int>()
     fun tribonacci(n: Int): Int {
