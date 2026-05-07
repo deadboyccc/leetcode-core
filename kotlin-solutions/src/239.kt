08c5244a289b78406e6dd5fefd3764fe58aefcb9
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-07 21:25:50 +0300

    239. Sliding Window Maximum

--- kotlin-solutions/src/239.kt
+++ kotlin-solutions/src/239.kt
@@ -1,5 +1,7 @@
 package two39
 
+import misc.kia_part_one.printSeperator
+
 class SolutionCleanButCosty {
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray = nums.toList()
         .windowed(k)
@@ -34,7 +36,69 @@ class Solution {
 
 fun main() {
     //[1,3,-1,-3,5,3,6,7]
-    println(Solution().maxSlidingWindow(intArrayOf(1, 3, -1, -3, 5, 3, 6, 7), 3).contentToString())
+//    println(Solution().maxSlidingWindow(intArrayOf(1, 3, -1, -3, 5, 3, 6, 7), 3).contentToString())
 
+    //The Best Test Case: [5, 2, 4, 3, 1]
+    printSeperator()
+    val testArray = intArrayOf(5, 2, 4, 3, 1)
+    IncreasingMonotonicStack(testArray)
+}
+
+class IncreasingMonotonicStack(val nums: IntArray) {
+    private val stack = ArrayDeque<Int>()
+
+    init {
+        for (num in nums) {
+            addIncreasing(num)
+        }
+    }
+
+    private fun addIncreasing(num: Int) {
+        println("--- Processing: $num ---")
+        printStack("Before      ")
+
+        while (stack.isNotEmpty() && stack.last() > num) {
+            val popped = stack.removeLast()
+            println("Intermediate: Popped $popped | Current Stack: $stack")
+        }
+
+        stack.addLast(num)
+        printStack("After       ")
+        println() // Extra line for readability between numbers
+    }
+
+    private fun printStack(prefix: String) {
+        println("$prefix: $stack")
+    }
+}
+
+class IncreasingMonotonicStackFancyPrint(val nums: IntArray) {
+    private val stack = ArrayDeque<Int>()
+
+    init {
+        println("%-10s | %-10s | %-20s | %s".format("Input", "Phase", "Stack State", "Action"))
+        println("-".repeat(60))
+        for (num in nums) {
+            process(num)
+        }
+    }
+
+    private fun process(num: Int) {
+        // Initial state for this specific number
+        printRow(num, "Start", stack.toString(), "Processing $num")
+
+        while (stack.isNotEmpty() && stack.last() > num) {
+            val popped = stack.removeLast()
+            printRow(num, "Pop", stack.toString(), "Popped $popped (>$num)")
+        }
+
+        stack.addLast(num)
+        printRow(num, "Push", stack.toString(), "Added $num")
+        println("-".repeat(60)) // Separator between numbers
+    }
+
+    private fun printRow(input: Int, phase: String, state: String, action: String) {
+        println("%-10d | %-10s | %-20s | %s".format(input, phase, state, action))
+    }
 }
 
