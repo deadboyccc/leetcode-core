d59e909fea3f9f80293d0cbf658416924d1f3c96
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-07 17:36:56 +0300

    355. Design Twitter

--- kotlin-solutions/src/355.kt
+++ kotlin-solutions/src/355.kt
@@ -1,23 +1,42 @@
 package three55
 
 class Twitter() {
-    // todo
+    var time = 0
+
+    // @ManyToMany
+    // map each userId to a list of its followers
+    val followers = mutableMapOf<Int, MutableList<Int>>()
+
+    // map each userId to a list of its tweets
+    val tweets = mutableMapOf<Int, MutableList<Int>>()
+
+    // everything is sequential
+    // maps tweetId -> sequence
+    val tweetDate = mutableMapOf<Int, Int>()
+
 
 
     fun postTweet(userId: Int, tweetId: Int) {
-
+        tweets.getOrDefault(userId, mutableListOf()).add(tweetId)
+        tweetDate[tweetId] = ++time
 
     }
 
     fun getNewsFeed(userId: Int): List<Int> {
+        // TODO
+
 
     }
 
     fun follow(followerId: Int, followeeId: Int) {
+        // followers -> followee
+        followers.getOrDefault(followeeId, mutableListOf()).add(followerId)
 
     }
 
     fun unfollow(followerId: Int, followeeId: Int) {
+
+        followers.getOrDefault(followeeId, mutableListOf()).remove(followerId)
 
     }
 
