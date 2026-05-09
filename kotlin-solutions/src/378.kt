abb334deed046715d91cbff6e9641a2f473ab794
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-09 12:29:26 +0300

    378. Kth Smallest in a sorted matrix

--- kotlin-solutions/src/378.kt
+++ kotlin-solutions/src/378.kt
@@ -269,6 +269,45 @@ class Solution {
     }
 }
 
+class PracticeSolution {
+    fun kthSmallest(matrix: Array<IntArray>, k: Int): Int {
+        var left = matrix.first().first()
+        var right = matrix.last().last()
+        val targetLessOrEqual = k - 1
+
+        while (left < right) {
+            // "Right-Biased" mid: when (left, right) are neighbors, this picks the higher one
+            // we are closing towards right, in the end if mid = right, then we check left=mid , and return the left
+            // which is mid which is at the right
+            // This prevents the infinite loop when setting left = mid
+            val mid = right - (right - left) / 2
+
+            // We count how many elements are STRICTLY LESS than mid
+            // If that count is <= target (k-1), then mid could be our answer or higher
+            if (countStrictlyLess(matrix, mid) <= targetLessOrEqual) {
+                left = mid
+            } else {
+                right = mid - 1
+            }
+        }
+        return left // which is mid
+    }
+
+    private fun countStrictlyLess(matrix: Array<IntArray>, target: Int): Int {
+        var count = 0
+        var row = matrix.size - 1
+        var col = 0
+        while (row >= 0 && col < matrix[0].size) {
+            if (matrix[row][col] < target) {
+                count += (row + 1)
+                col++
+            } else {
+                row--
+            }
+        }
+        return count
+    }
+}
 // ========================================================
 // Main
 // ========================================================
