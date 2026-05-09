f2c405bee1cd5a94683b34f45dd70bbd25ad57ee
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-09 18:46:55 +0300

    chore: stop tracking Gradle/build generated artifacts

--- kotlin-solutions/src/main/kotlin/misc/CompanianObjects.kt
+++ kotlin-solutions/src/main/kotlin/misc/CompanianObjects.kt
@@ -21,11 +21,32 @@ class Person(val name: String, val age: Int) {
     }
 }
 
+@Serializable
+class ExtensionFunctionsPerson(val name: String, val age: Int) {
+    override fun toString(): String {
+        return "$name, $age"
+    }
+
+    companion object {
+        fun test(): Unit {}
+    }
+}
+
+fun ExtensionFunctionsPerson.Companion.toJSON(jsonText: String): Person {
+    return Json.decodeFromString<Person>(jsonText)
+}
+
+
+
 
 fun main() {
-    val r1 = Random.Default.nextInt()
+
+    val r1 = Random.Default.nextInt(18, 100)
+    println("r1=$r1")
     val r2 = Random.nextInt()
 
     val person = Person.fromJSON("""{"name": "Bob", "age": 25}""")
+    val personExtensionFunctions = ExtensionFunctionsPerson("test", r1.coerceIn(18..35))
+    println(personExtensionFunctions)
     println(person.toString())
 }
