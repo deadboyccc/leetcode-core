9e895bfe859d9051db356f6d11326f2d7b43cd27
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-05-14 15:33:21 +0300

    200. Number of Islands

--- kotlin-solutions/src/main/kotlin/200.kt
+++ kotlin-solutions/src/main/kotlin/200.kt
@@ -1,5 +1,60 @@
 package two00;
 
+class ConnectedComponents {
+    class Solution {
+        fun numIslands(grid: Array<CharArray>): Int {
+            if (grid.isEmpty()) return 0
+
+            val rows = grid.indices
+            val cols = grid[0].indices
+            val visited = mutableSetOf<Pair<Int, Int>>()
+            var islandCount = 0
+
+            // Idiomatic grid traversal
+            for (r in rows) {
+                for (c in cols) {
+                    // If we find land and haven't been here, it's a new island
+                    if (grid[r][c] == '1' && (r to c) !in visited) {
+                        islandCount++
+                        exploreIsland(grid, r, c, visited)
+                    }
+                }
+            }
+            return islandCount
+        }
+
+        private fun exploreIsland(
+            grid: Array<CharArray>,
+            startR: Int,
+            startC: Int,
+            visited: MutableSet<Pair<Int, Int>>
+        ) {
+            val directions = listOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+            val queue = ArrayDeque<Pair<Int, Int>>().apply { add(startR to startC) }
+            visited.add(startR to startC)
+
+            while (queue.isNotEmpty()) {
+                val (currR, currC) = queue.removeFirst()
+
+                for ((dr, dc) in directions) {
+                    val nr = currR + dr
+                    val nc = currC + dc
+
+                    // Idiomatic boundary and condition check
+                    if (nr in grid.indices &&
+                        nc in grid[0].indices &&
+                        grid[nr][nc] == '1' &&
+                        (nr to nc) !in visited
+                    ) {
+                        visited.add(nr to nc)
+                        queue.add(nr to nc)
+                    }
+                }
+            }
+        }
+    }
+}
+
 class Solution {
     fun numIslands(grid: Array<CharArray>): Int {
 
